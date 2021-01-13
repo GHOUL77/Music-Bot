01958261d05f12100c7320b05af41d94c2363cc7
GHOUL77 <76803897+GHOUL77@users.noreply.github.com>
2021-01-13 19:01:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.Token = ""; //Bot Token - Important
-exports.Owner = ""; //Bot Owner ID - Important
-exports.Default_Prefix = ""; //Bot Default Prefix (Examples: A!, !)- Important
-exports.Color = ""; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
-exports.Support = ``; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
+exports.Token = "Nzk0ODk0NDUwNTIxNDA3NTA4.X_BdJQ.NeZPX_a4dKsfnws9pMOZGRmh5Eo"; //Bot Token - Important
+exports.Owner = "Ghoul#5480"; //Bot Owner ID - Important
+exports.Default_Prefix = "-"; //Bot Default Prefix (Examples: A!, !)- Important
+exports.Color = "BLUE"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
+exports.Support = `https://discord.gg/khQSV8XNAu`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
